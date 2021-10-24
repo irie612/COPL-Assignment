a8b53c8e57bef9708e9b5a7a22d8708658987438
emanuele <e.greco@umail.leidenuniv.nl>
2021-10-25 00:50:08 +0200

    substituteTree now create a new indipendent subtree

--- AST.go
+++ AST.go
@@ -37,10 +37,14 @@ func newNode(value string, token int) *node {
 
 func (n *node) linkNodes(child ...*node) {
 	n.left = child[0]
-	child[0].parent = n
+	if child[0] != nil {
+		child[0].parent = n
+	}
 	if len(child) > 1 {
 		n.right = child[1]
-		child[1].parent = n
+		if child[1] != nil {
+			child[1].parent = n
+		}
 	}
 }
 
@@ -192,54 +196,83 @@ func giveFresh(theSlice []rune, duplicateVar rune) rune {
 	return freshChar
 }
 
-func alphaConversion(theNode *node, duplicateVar rune, freshChar rune) {
+//starting from theNode it searches for variables and lambda expr with value duplicateVar.
+//it will replace the value of those nodes with freshChar
+//a flag is needed to ensure that if it founds another lambda expr with the same variable name it will return
+//and stop the conversion. this way only the variables bounded to the first lambda expr are renamed
+//we are using a top-down approach, without actually binding the variables to lambda expression they are bounded to.
+func alphaConversion(theNode *node, duplicateVar rune, freshVar rune, flag bool) {
 	if (theNode == nil) {
 		return
 	}
-
+	local_flag := flag
 	nodeVar := []rune(theNode.value)
-	if ((theNode.token == LAMBDA || theNode.token == VARIABLE) && nodeVar[0] == duplicateVar) {
-		theNode.value = string(freshChar)
+
+	if nodeVar[0] == duplicateVar{
+		switch theNode.token {
+		case LAMBDA:
+			if flag{
+				return
+			}
+			//else
+			local_flag = true
+			fallthrough
+		case VARIABLE:
+			theNode.value = string(freshVar)
+		}
 	}
 
-	alphaConversion(theNode.left, duplicateVar, freshChar)
-	alphaConversion(theNode.right, duplicateVar, freshChar)
+	alphaConversion(theNode.left, duplicateVar, freshVar,local_flag)
+	alphaConversion(theNode.right, duplicateVar, freshVar,local_flag)
 }
 
 //*************************************************************************
 
 
 // substitutes the variables bound to the original lambda expression.
-// however, im unsure whether newNode is actually a new independent node
-// from subNode (subnode= the "values" for substitution), or if it a some sort of pointer
-// to the subNode. As in, every substituted node are all merely pointing to the single subNode.
-func substituteTree(theNode *node, subNode *node, targetVar rune) {
+// newNode is new independent node
+// not sure if would work with only one right handside tree that's being pointed by the parents of every bound variable
+func substituteTree(theNode *node, subNode *node, targetVar string) {
 	if (theNode == nil) {
 		return
 	}
 
-	nodeVar := []rune(theNode.value)
-	if (theNode.token == LAMBDA && nodeVar[0] == targetVar) {
+	//nodeVar := []rune(theNode.value)
+	if (theNode.token == LAMBDA && theNode.value == targetVar) {
 		return
 	}
 
-	if (theNode.token == VARIABLE && nodeVar[0] == targetVar) {	
-		var newNode *node
-		newNode = subNode
+	if (theNode.token == VARIABLE && theNode.value == targetVar) {
+		//var newNode *node
+		//newNode = subNode
+		newNode := getCopySubtree(subNode)
 		parentNode := theNode.parent
 		if (parentNode.left == theNode) {
 			parentNode.left = newNode
-			newNode.parent = parentNode
-			theNode = newNode
 		} else if (parentNode.right == theNode) {
 			parentNode.right = newNode
-			newNode.parent = parentNode
-			theNode = newNode
+		} else{
+			print ("SHOULDN'T BE HERE\n")
+			os.Exit(1)
 		}
+		newNode.parent = parentNode
+		//theNode = newNode
 	}
 
 	substituteTree(theNode.left, subNode, targetVar)
 	substituteTree(theNode.right, subNode, targetVar)
+}
+
+//get an exact copy of subtree. but every node is created again = indipendent
+func getCopySubtree(subtree *node) *node{
+	//base case
+	if subtree == nil{
+		return nil
+	}
+	//Create a new identical node and then link a copy of the left and the right of the subtree
+	returnNode := newNode(subtree.value,subtree.token)
+	returnNode.linkNodes(getCopySubtree(subtree.left) , getCopySubtree(subtree.right))
+	return returnNode
 }
 
 
@@ -267,15 +300,15 @@ func applyBetaReduc(theNode **node) bool {
 			
 			usedVars := append(boundVars, freeVars...)
 			freshVar := giveFresh(usedVars, duplicateVar)
-			alphaConversion((*theNode).left, duplicateVar, freshVar)
+			alphaConversion((*theNode).left, duplicateVar, freshVar, false)
 
 			giveBoundVars((*theNode).left, &boundVars)
 			giveFreeVars((*theNode).right, &freeVars)
 			varIsCopy, _ = isCopy(boundVars, freeVars)
 		}
 		//actual substitution
-		targetSplice := []rune((*theNode).left.value)
-		targetVar := targetSplice[0]
+		targetVar := ((*theNode).left.value)
+		//targetVar := targetSplice[0]
 		substituteTree((*theNode).left.left, (*theNode).right, targetVar)	
 
 		//link the sub tree of the lambda node to the its parent
@@ -283,30 +316,23 @@ func applyBetaReduc(theNode **node) bool {
 		//change the value stored at the address of the lambda node
 		**theNode = *((*theNode).left.left)
 
-		//Reduction is not possible. Check left branch. If no reduction found there, check right side
+		return true
 	}else{
+		//Reduction is not possible. Check left branch. If no reduction found there, check right side
 		if applyBetaReduc( (&(*theNode).left) ) == false {
 			return applyBetaReduc((&(*theNode).right))
-		}else{
+		} else{
 			return true
 		}
 	}
 
 	return false
-} 
-
-
-
-/*func betaReduc(theNode *node) {
-	if (applyBetaReduc(theNode)) {
-		
-	}
-}*/
+}
 
 // this function doesnt really have a functionality yet.
 // I am merely using it to test functions with.
 func checkReduction (theNode **node) bool {
-
+	/*
 	boundVars := []rune{}
 	giveBoundVars((*theNode), &boundVars)
 	for i := range boundVars {
@@ -320,12 +346,10 @@ func checkReduction (theNode **node) bool {
 		theByte := freeVars[i]
 		fmt.Fprintf(os.Stdout,  "free %c \n", theByte)
 	}
-
+	*/
 	if applyBetaReduc(&(*theNode)) {
-		fmt.Fprintf(os.Stdout, "apply work")
+		fmt.Fprintf(os.Stdout, "apply work\n")
 	}
-	
-	
 	return true
 } 
 
